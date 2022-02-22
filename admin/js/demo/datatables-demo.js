b5fff69cb3a50de22eb33d021819e59f6239977a
Emerald2240 <Orjimichael4886@gmail.com>
2022-02-22 22:12:41 +0100

    change dataTables calling function and made it show all entries by default

--- admin/js/demo/datatables-demo.js
+++ admin/js/demo/datatables-demo.js
@@ -1,4 +1,13 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable( {
+            "lengthMenu": [[-1, 10, 25, 50 ], ["All", 10, 25, 50 ]]
+          } );
 });
+
+
+// $(document).ready( function() {
+//   *      $('#example').dataTable( {
+//   *        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
+//   *      } );
+//   *    } );
